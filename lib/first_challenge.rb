e022d8b01dacca6b8311c981669ae1951a03e810
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-02-12 19:29:33 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,6 +14,9 @@ def first_challenge
   }
 
   #your code here
+  contacts["Freddy Mercury"][:favorite_icecream_flavors].reject do |flavor|
+    flavor == "strawberry"
+  end
 
 
   #remember to return your newly altered contacts hash!
